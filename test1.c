e958e3eb4651c6ec72f4dd916aef43623c2eca20
b00902130 <b00902130@ntu.edu.tw>
2014-05-09 21:00:14 +0800

    Add some books.

--- test1.c
+++ test1.c
@@ -16,16 +16,18 @@ int main()
 	while(fgets(line,sizeof(line),fp)!=NULL)
 	{
 		sscanf(line,"%s %d\n",temp_book,&state);
-		fseek(fp, strlen(line), SEEK_CUR);
+		// fseek(fp, strlen(line), SEEK_CUR);
 		if(strcmp(book,temp_book)==0)
 		{
+			sprintf(changeString, "%s %d\n", book,-1);
 			// strlen(temp_book);
-			fseek(fp,0,SEEK_CUR);
-			strncpy(changeString,delete_string,strlen(line));
-			// strcpy
-			changeString[strlen(line)] = '\n';
-			// changeString[strlen(line)+1] = '\0';
-			fwrite(changeString,sizeof(char),strlen(line)-1,fp);
+			// fseek(fp,0,SEEK_CUR);
+			// strncpy(changeString,delete_string,strlen(line));
+			// // strcpy
+			// changeString[strlen(line)] = '\n';
+			// // changeString[strlen(line)+1] = '\0';
+			fseek(fp, -strlen(line), SEEK_CUR);
+			fwrite(changeString,sizeof(char),strlen(line),fp);
 		}
 	}
 	fclose(fp);
