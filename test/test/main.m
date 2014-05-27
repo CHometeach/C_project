f223d97ac5e1fa442c652c2fe357f4d2d1739dec
seanlin <seanlin@seanlindeMacBook-Pro.local>
2014-05-27 21:18:04 +0800

    first commit

---
+++ test/test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by seanlin on 2014/5/27.
+//  Copyright (c) 2014年 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
